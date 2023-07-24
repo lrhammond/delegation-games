8a65b175d1bf27cc8ce7e19b59b2ba74d9e7dc64
Oliver Sourbut <olyis@live.co.uk>
2023-07-24 14:27:18 +0100

    Support generating non-uniformly-scaled games

--- generate_games.py
+++ generate_games.py
@@ -23,16 +23,16 @@ def game_to_flatgame(g:Game) -> FlatGame:
 def flatgame_to_game(g:FlatGame) -> Game:
     return Game(*(a.reshape((2, 2)) for a in g))
 
-def generate_delegation_games():
+def generate_delegation_games(m1=1., m2=1.):
     '''
     Yield infinite stream of uniformly-generated standardised delegation games
     '''
     def payoff():
         return geom.standardise(games.uniform_2x2_payoff().flatten())
     while True:
-        yield DelegationGame(FlatGame(payoff(), payoff()), FlatGame(payoff(), payoff()))
+        yield DelegationGame(FlatGame(payoff(), payoff()), FlatGame(m1*payoff(), m2*payoff()))
 
-def generate_delegation_games_with_alignment_bounds(max_epic=.2, min_epic=.0):
+def generate_delegation_games_with_alignment_bounds(max_epic=.2, min_epic=.0, m1=1., m2=1.):
     '''
     Yield infinite stream of delegations games where
     - principals are uniformly-generated
@@ -44,6 +44,6 @@ def generate_delegation_games_with_alignment_bounds(max_epic=.2, min_epic=.0):
         epics = np.random.uniform(min_epic, max_epic, 2)
         principals = FlatGame(payoff(), payoff())
         agents = FlatGame(
-            geom.random_epic_distance_step(principals[0], epics[0]),
-            geom.random_epic_distance_step(principals[1], epics[1]))
+            m1*geom.random_epic_distance_step(principals[0], epics[0]),
+            m2*geom.random_epic_distance_step(principals[1], epics[1]))
         yield DelegationGame(principals, agents)
